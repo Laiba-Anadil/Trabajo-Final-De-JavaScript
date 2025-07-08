7e4b591987b7a876f0935dec0488f438d3e1fd52
Laiba-Anadil <juttcheema057@gmail.com>
2025-07-08 15:32:17 +0200

    Trabajo final de proyecto con javaScript

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,20 @@
 //Noticias de HTML
-fetch('noticias.json')
-  .then(response => response.json())
-  .then(data => {
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'noticias.json', true);
+
+xhr.onload = function() {
+  if (xhr.status === 200) {
+    const data = JSON.parse(xhr.responseText);
     let html = '';
     data.noticias.forEach(noticia => {
-      html += "<div class = 'noticia'>";
+      html += "<div class='noticia'>";
       html += "<strong>" + noticia.titulo + "</strong><br>";
       html += noticia.contenido;
       html += "</div>";
     });
     document.getElementById('noticias').innerHTML = html;
-  });
+  }
+};
+
+xhr.send();
+
